1fcbbf534f1e5a09e0697af2cc0ce43ae52f67c8
J7ohn <jvfonseca1349@gmail.com>
2024-06-25 00:52:32 -0300

    incrementando botões

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,34 @@ main(){
 
 class PerguntaApp extends StatelessWidget{
 
+  
+
   @override
   Widget build(BuildContext context){
-    return new MaterialApp(
+
+    final List<String> perguntas = [
+      'Qual é sua cor preferida?',
+      'Qual é seu animal favorito?'
+    ];
+
+    return MaterialApp(
+
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá mundo!'),
+
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(child: Text('Resposta 1'), onPressed: null,),
+            ElevatedButton(child: Text('Resposta 2'), onPressed: null,),
+            ElevatedButton(child: Text('Resposta 3'), onPressed: null,)
+          ],
+        ),
+
       ),
+
     );
   }
 }
